397ef75ca1ba2dcbc321cc378951a9714317e1f0
JiuXiaoY <105855187+JiuXiaoY@users.noreply.github.com>
2024-06-21 15:03:33 +0800

    这是一个后端初始化模板

---
+++ imagedada-backend/src/main/java/com/image/project/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,30 @@
+package com.image.project.model.dto.postfavour;
+
+import com.image.project.common.PageRequest;
+import com.image.project.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
